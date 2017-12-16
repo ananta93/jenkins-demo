347f45a69a9f3734b556f8948a9a5996243783fd
Ananta Chandra Das <anantadas.dilan@gmail.com>
2017-12-16 20:52:14 +0530

    another file

---
+++ src/main/java/com/teclearners/jenkins/AddTwoNumbers.java
@@ -0,0 +1,9 @@
+package com.teclearners.jenkins;
+
+public class AddTwoNumbers {
+	public static void main(String[] args) {
+		int x=10;
+		int y=20;
+		System.out.println("Addition of the numbers : "+(x+y));
+	}
+}
--- src/main/java/com/teclearners/jenkins/HelloWorld.java
+++ src/main/java/com/teclearners/jenkins/HelloWorld.java
@@ -1,4 +1,4 @@
-package com.teclearners.jenikins;
+package com.teclearners.jenkins;
 
 public class HelloWorld {
 	public static void main(String[] args) {
